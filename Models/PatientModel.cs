d6f753cb70a47898825c249498f76edb6c297eb0
ludevinparkinson@gmail.com <41333623+lrparkinson@users.noreply.github.com>
2021-04-21 08:43:08 +0200

    Added Form For Stroke Prediction

--- Models/PatientModel.cs
+++ Models/PatientModel.cs
@@ -2,20 +2,36 @@
 
 namespace ClassificationApp.Models
 {
+
+    public enum Gender
+    {
+        Unknown,
+        Male,
+        Female,
+        Other
+    }
+
     public class PatientModel
     {
         [Required]
         public int Id { get; set; }
         [Required]
+        [Display(Name = "First Name")]
         [MaxLength(250)]
         public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Middle Name")]
+        [MaxLength(250)]
         public string MiddleName { get; set; }
         [Required]
+        [Display(Name = "Last Name")]
         [MaxLength(250)]
         public string LastName { get; set; }
         [Required]
+        [Range(0, int.MaxValue)]
         public int Age { get; set; }
         [Required]
-        public string Gender { get; set; }
+        [EnumDataType(typeof(Gender))]
+        public Gender Gender { get; set; }
     }
 }
